cbb7c226aeb4f53c4d1f848742e28ebf457a52d1
Peregon Bogdan <peregonbw@gmail.com>
2020-03-28 11:58:39 +0200

    first and last commit

---
+++ app/js/app.js
@@ -0,0 +1,26 @@
+$(window).on('load resize', () => {
+    (function carousel() {
+        let $carousel = $('#carousel');
+        if ($carousel.length) {
+            $carousel.fullpage({
+                navigation: false,
+            })
+        }
+    })();
+
+    (function error() {
+        let $error = $('.error');
+        $(window).height() <= $(window).width() ?
+            $error.addClass('active') :
+            $error.removeClass('active');
+    })();
+
+    (function menu() {
+        let $menu = $('.menu');
+        $('.header-menu').on('click', () => {
+            $menu.hasClass('active') ?
+                $menu.removeClass('active') :
+                $menu.addClass('active')
+        });
+    })();
+});
